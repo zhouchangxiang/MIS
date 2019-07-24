fd65cecb0b2db90b35b2d174afef89dd12ee2152
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2019-07-24 11:02:54 +0800

    no message

--- MICS_Server.py
+++ MICS_Server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, abort, request, jsonify, render_template
+from flask_restful import reqparse, abort, Api, Resource
+import json
 from flask_login import login_required
 from dbset.account import auth_lib
 from handlers.account import account_auth
@@ -45,8 +47,66 @@ app.register_blueprint(batch)
 
 @app.route('/')
 @login_required
-def hello_world():
-    return render_template("./main/main.html")
+def index():
+    return render_template("./main/index.html")
+app = Flask(__name__)
+api = Api(app)
 
-if __name__ == "__main__":
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '哈哈哈'},
+    'todo3': {'task': 'profit!'},
+}
+
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
+if __name__ == '__main__':
     app.run(debug=True)
+
