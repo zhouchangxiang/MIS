4becf8fe5a875aab2dd4b80f0528aca34c2939e6
lenux-PC\lenux <minwork0926@163.com>
2019-08-20 11:31:54 +0800

    no message

--- static/script/admin.js
+++ static/script/admin.js
@@ -71,7 +71,9 @@ $(function () {
 			success: function(res) {
 				res = JSON.parse(res)
 				console.log(res.rows[0].session_id)
-				if(res.rows[0].session_id != getCookie("sesseionid")){
+				var dataSesseionid = getCookie("sesseionid")
+				console.log(dataSesseionid)
+				if(res.rows[0].session_id != dataSesseionid){
 					bootbox.alert({
 						message: "您的账号已在其他设备登录，请重新登录",
 						callback: function () {
@@ -259,14 +261,22 @@ function setCookie(name,value,iDay){
 		oDate.setDate(oDate.getDate()+iDay);
 	document.cookie=name+'='+value+';expires='+oDate;
 }
-function getCookie(key){
-	console.log(document.cookie)
-	var startIndex = document.cookie.indexOf(name); //开始索引
-    if (startIndex > -1) {
-            var tempStr = document.cookie.substring(startIndex, document.cookie.length);
-        var endIndex = tempStr.indexOf(";")//结束索引：第一个分号 索引
-        var item = tempStr.substring(0, endIndex);
-        return item.split("=")[1];
-    }
-    return "";
+function getCookie(cookie_name){
+	var allcookies = document.cookie;
+	//索引长度，开始索引的位置
+	var cookie_pos = allcookies.indexOf(cookie_name);
+	// 如果找到了索引，就代表cookie存在,否则不存在
+	if (cookie_pos != -1) {
+		// 把cookie_pos放在值的开始，只要给值加1即可
+		//计算取cookie值得开始索引，加的1为“=”
+		cookie_pos = cookie_pos + cookie_name.length + 1;
+		//计算取cookie值得结束索引
+		var cookie_end = allcookies.indexOf(";", cookie_pos);
+		if (cookie_end == -1) {
+			cookie_end = allcookies.length;
+		}
+		//得到想要的cookie的值
+		var value = unescape(allcookies.substring(cookie_pos, cookie_end));
+	}
+	return value;
 }
