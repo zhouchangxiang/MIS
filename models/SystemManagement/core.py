96218ee1d0b5a9ed4dce658bc248a000c6725985
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2019-09-10 10:29:05 +0800

    no message

--- models/SystemManagement/core.py
+++ models/SystemManagement/core.py
@@ -97,81 +97,6 @@ class Role(Base):
 
 
 
-#User_START:
-class User(Base):
-	__tablename__ = "User" 
-	
-	#id:
-	id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
-	
-	#用户名:
-	Name = Column(Unicode(64), primary_key = False, autoincrement = False, nullable = True)
-	
-	#密码:
-	Password = Column(Unicode(128), primary_key = False, autoincrement = False, nullable = True)
-	
-	#工号:
-	WorkNumber = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
-	
-	#登录状态:
-	Status = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
-	
-	#创建用户:
-	Creater = Column(Unicode(65), primary_key = False, autoincrement = False, nullable = True)
-	
-	#创建时间:
-	CreateTime = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
-	
-	#上次登录时间:
-	LastLoginTime = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
-	
-	#是否锁定:
-	IsLock = Column(BIT, primary_key = False, autoincrement = False, nullable = True)
-	
-	#所属厂区:
-	FactoryName = Column(Unicode(65), primary_key = False, autoincrement = False, nullable = True)
-	
-	#所属部门:
-	OrganizationName = Column(Unicode(50), primary_key = False, autoincrement = False, nullable = True)
-	
-	#角色名称:
-	RoleName = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
-	
-	#角色编码:
-	RoleCode = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
-
-	# session_id:
-	session_id = Column(Unicode(32), primary_key=False, autoincrement=False, nullable=True)
-
-	# @property
-	# def password(self):
-	#     raise AttributeError('password is not a readable attribute')
-
-	# 定义password字段的写方法，我们调用generate_password_hash将明文密码password转成密文Shadow
-	# @password.setter
-	def password(self, password):
-		self.Password = generate_password_hash(password)
-		return self.Password
-
-	# 定义验证密码的函数confirm_password
-	def confirm_password(self, password):
-		return check_password_hash(self.Password, password)
-
-	@property
-	def is_authenticated(self):
-		return True
-
-	@property
-	def is_active(self):
-		return True
-
-	@property
-	def is_anonymous(self):
-		return False
-
-	def get_id(self):
-		return str(self.id)  # python 3
-#User_END:
 
 
 
@@ -527,6 +452,87 @@ class Equipment(Base):
 	EQPCode = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
 	
 #Equipment_END:
+
+
+#User_START:
+class User(Base):
+	__tablename__ = "User" 
+	
+	#id:
+	id = Column(Integer, primary_key = True, autoincrement = True, nullable = False)
+
+	# 用户名:
+	Name = Column(Unicode(64), primary_key=False, autoincrement=False, nullable=True)
+
+	# 密码:
+	Password = Column(Unicode(32), primary_key=False, autoincrement=False, nullable=True)
+
+	# 工号:
+	WorkNumber = Column(Unicode(32), primary_key=False, autoincrement=False, nullable=True)
+	
+	#所属岗位:
+	StationName = Column(Unicode(65), primary_key = False, autoincrement = False, nullable = True)
+
+	# 登录状态:
+	Status = Column(Unicode(32), primary_key=False, autoincrement=False, nullable=True)
+
+	# session_id:
+	session_id = Column(Unicode(32), primary_key=False, autoincrement=False, nullable=True)
+
+	#角色编码:
+	RoleCode = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
+	
+	#角色名称:
+	RoleName = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
+	
+	#所属部门:
+	OrganizationName = Column(Unicode(50), primary_key = False, autoincrement = False, nullable = True)
+	
+	#所属厂区:
+	FactoryName = Column(Unicode(65), primary_key = False, autoincrement = False, nullable = True)
+	
+	#上次登录时间:
+	LastLoginTime = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
+	
+	#创建时间:
+	CreateTime = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
+	
+	#创建用户:
+	Creater = Column(Unicode(65), primary_key = False, autoincrement = False, nullable = True)
+
+	# 是否锁定:
+	IsLock = Column(BIT, primary_key=False, autoincrement=False, nullable=True)
+
+	# @property
+	# def password(self):
+	#     raise AttributeError('password is not a readable attribute')
+
+	# 定义password字段的写方法，我们调用generate_password_hash将明文密码password转成密文Shadow
+	# @password.setter
+	def password(self, password):
+		self.Password = generate_password_hash(password)
+		return self.Password
+
+	# 定义验证密码的函数confirm_password
+	def confirm_password(self, password):
+		return check_password_hash(self.Password, password)
+
+	@property
+	def is_authenticated(self):
+		return True
+
+	@property
+	def is_active(self):
+		return True
+
+	@property
+	def is_anonymous(self):
+		return False
+
+	def get_id(self):
+		return str(self.id)  # python 3
+	
+#User_END:
 
 # 生成表单的执行语句_START
 Base.metadata.create_all(engine)
