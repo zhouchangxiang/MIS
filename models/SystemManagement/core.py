dc07f58b81df5974bd5e47b4cbf3bf61cef50e92
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2019-09-24 15:16:00 +0800

    no message

--- models/SystemManagement/core.py
+++ models/SystemManagement/core.py
@@ -799,7 +799,7 @@ class Equipment(Base):
 	SuperVisor = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
 	
 	#操作时间:
-	OperationDate = Column(Unicode(OperationDate), primary_key = False, autoincrement = False, nullable = True)
+	OperationDate = Column(Unicode(32), primary_key = False, autoincrement = False, nullable = True)
 	
 	#车间:
 	WorkShop = Column(Unicode(65), primary_key = False, autoincrement = False, nullable = True)
