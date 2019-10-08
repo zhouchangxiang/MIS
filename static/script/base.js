d987ad4fa99f49c001b115adf0e8c119bb3a669b
lenux-PC\lenux <minwork0926@163.com>
2019-10-08 09:04:09 +0800

    no message

--- static/script/base.js
+++ static/script/base.js
@@ -215,6 +215,30 @@ function Digit(a) {
         i.timerTemp -= i.single, b(a.dom, a.number), c(a.dom, i, d)
     }
 }
+//highchart全局配置
+function HighchartsGlobalSettings(){
+    Highcharts.setOptions({
+        lang:{
+           contextButtonTitle:"图表导出菜单",
+           decimalPoint:".",
+           downloadJPEG:"下载JPEG图片",
+           downloadPDF:"下载PDF文件",
+           downloadPNG:"下载PNG文件",
+           downloadSVG:"下载SVG文件",
+           drillUpText:"返回 {series.name}",
+           loading:"加载中",
+           months:["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
+           noData:"没有数据",
+           numericSymbols: [ "千" , "兆" , "G" , "T" , "P" , "E"],
+           printChart:"打印图表",
+           resetZoom:"恢复缩放",
+           resetZoomTitle:"恢复图表",
+           shortMonths: [ "Jan" , "Feb" , "Mar" , "Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"],
+           thousandsSep:",",
+           weekdays: ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六","星期天"]
+        }
+    });
+}
 //highchart静态图表渲染方法
 function highchartsRender(id,xAxisArray,seriesData){
     var chart = Highcharts.chart(id,{
