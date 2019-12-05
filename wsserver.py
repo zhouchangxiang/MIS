c76688524a2baedc3c60b1f24f5f9c454d950418
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2019-12-05 17:14:07 +0800

    no message

--- wsserver.py
+++ wsserver.py
@@ -139,14 +139,12 @@ def handler_msg(conn):
                                                                                  tag.TagClassValue + "F"))
                             tis["Sum"] = strtofloat(redis_conn.hget(constant.REDIS_TABLENAME,
                                                                                  tag.TagClassValue + "S"))
-                            dis.append(tis)
                             data_dict["S"] = dis
                         elif S == "W":
                             tiw["Flow"] = strtofloat(redis_conn.hget(constant.REDIS_TABLENAME,
                                                                                  tag.TagClassValue + "F"))
                             tiw["Sum"] = strtofloat(redis_conn.hget(constant.REDIS_TABLENAME,
                                                                                  tag.TagClassValue + "S"))
-                            diw.append(tiw)
                             data_dict["W"] = diw
                         elif S == "E":
                             tie["ZGL"] = strtofloat(redis_conn.hget(constant.REDIS_TABLENAME,
@@ -163,7 +161,6 @@ def handler_msg(conn):
                                                                                   tag.TagClassValue + "CU"))
                             tie["CI"] = strtofloat(redis_conn.hget(constant.REDIS_TABLENAME,
                                                                                   tag.TagClassValue + "CI"))
-                            die.append(tie)
                             data_dict["E"] = die
                     except Exception as ee:
                         print("报错tag：" + tag.TagClassValue + " |报错IP：" + tag.IP + "  |报错端口：" + tag.COMNum + "  |错误：" + str(ee))
