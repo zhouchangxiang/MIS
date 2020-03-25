090c27793c9e07f96fa2fb223c93002e920ae424
thinkpad-PC\thinkpad <minwork0926@163.com>
2020-03-25 16:07:11 +0800

    no message

--- redis_incremeninsertdb_server.py
+++ redis_incremeninsertdb_server.py
@@ -13,7 +13,7 @@ from dbset.database.db_operate import db_session, pool
 from dbset.main.BSFramwork import AlchemyEncoder
 from flask_login import login_required, logout_user, login_user, current_user, LoginManager
 import arrow
-from models.SystemManagement.core import RedisKey, TagClassType, ElectricEnergy, Unit, PriceList, SteamEnergy, \
+from models.SystemManagement.core import RedisKey, TagClassType, ElectricEnergy, Unit, SteamEnergy, \
     WaterEnergy, TagDetail, Equipment
 from models.SystemManagement.system import EarlyWarningLimitMaintain, EarlyWarning, EarlyWarningPercentMaintain, \
     ElectricPrice, IncrementTable, WaterSteamPrice
@@ -24,10 +24,10 @@ from dbset.log.BK2TLogger import logger, insertSyslog
 
 def run():
     while True:
-        time.sleep(60)
+        # time.sleep(60)
         print("数据开始写入增量数据库")
         try:
-            elekeys = db_session.query(ElectricEnergy).filter(ElectricEnergy.IncremenFlag == "0").all()
+            elekeys = db_session.query(ElectricEnergy).filter(ElectricEnergy.IncrementFlag == "0").order_by(("ID")).all()
             for key in elekeys:
                 inc = IncrementTable()
                 inc.TagClassValue = key.TagClassValue
@@ -35,8 +35,12 @@ def run():
                 inc.CollectionMonth = key.CollectionMonth
                 inc.CollectionDay = key.CollectionDay
                 inc.CollectionHour = str(key.CollectionDate)[0:13]
-                prozgl = db_session.query(ElectricEnergy.ZGL).filter(ElectricEnergy.ID == key.PrevID).first()[0]
-                inc.IncremenValue = key.ZGL - prozgl
+                prozgl = db_session.query(ElectricEnergy.ZGL).filter(ElectricEnergy.ID == key.PrevID).first()
+                if prozgl != None:
+                    prozgl = prozgl[0]
+                else:
+                    prozgl = 0
+                inc.IncremenValue = round(float(key.ZGL) - float(prozgl), 2)
                 inc.IncremenType = "电"
                 inc.CollectionDate = key.CollectionDate
                 inc.Unit = key.Unit
@@ -46,7 +50,7 @@ def run():
                 db_session.add(inc)
                 key.IncrementFlag = "1"
                 db_session.commit()
-            watkeys = db_session.query(WaterEnergy).filter(WaterEnergy.IncremenFlag == "0").all()
+            watkeys = db_session.query(WaterEnergy).filter(WaterEnergy.IncrementFlag == "0").order_by(("ID")).all()
             for key in watkeys:
                 inc = IncrementTable()
                 inc.TagClassValue = key.TagClassValue
@@ -54,18 +58,22 @@ def run():
                 inc.CollectionMonth = key.CollectionMonth
                 inc.CollectionDay = key.CollectionDay
                 inc.CollectionHour = str(key.CollectionDate)[0:13]
-                proWaterSum = db_session.query(WaterEnergy.WaterSum).filter(WaterEnergy.ID == key.PrevID).first()[0]
-                inc.IncremenValue = key.WaterSum - proWaterSum
+                proWaterSum = db_session.query(WaterEnergy.WaterSum).filter(WaterEnergy.ID == key.PrevID).first()
+                if proWaterSum != None:
+                    proWaterSum = proWaterSum[0]
+                else:
+                    proWaterSum = 0
+                inc.IncremenValue = round(float(key.WaterSum) - float(proWaterSum), 2)
                 inc.IncremenType = "水"
                 inc.CollectionDate = key.CollectionDate
-                inc.Unit = key.Unit
+                inc.Unit = key.SumWUnit
                 inc.EquipmnetID = key.EquipmnetID
                 inc.PriceID = key.PriceID
                 inc.AreaName = key.AreaName
                 db_session.add(inc)
                 key.IncrementFlag = "1"
                 db_session.commit()
-            stekeys = db_session.query(SteamEnergy).filter(SteamEnergy.IncremenFlag == "0").all()
+            stekeys = db_session.query(SteamEnergy).filter(SteamEnergy.IncrementFlag == "0").order_by(("ID")).all()
             for key in stekeys:
                 inc = IncrementTable()
                 inc.TagClassValue = key.TagClassValue
@@ -73,11 +81,15 @@ def run():
                 inc.CollectionMonth = key.CollectionMonth
                 inc.CollectionDay = key.CollectionDay
                 inc.CollectionHour = str(key.CollectionDate)[0:13]
-                proSumValue = db_session.query(SteamEnergy.SumValue).filter(SteamEnergy.ID == key.PrevID).first()[0]
-                inc.IncremenValue = key.SumValue - proSumValue
+                proSumValue = db_session.query(SteamEnergy.SumValue).filter(SteamEnergy.ID == key.PrevID).first()
+                if proSumValue != None:
+                    proSumValue = proSumValue[0]
+                else:
+                    proSumValue = 0
+                inc.IncremenValue = round(float(key.SumValue) - float(proSumValue), 2)
                 inc.IncremenType = "汽"
                 inc.CollectionDate = key.CollectionDate
-                inc.Unit = key.Unit
+                inc.Unit = key.SumUnit
                 inc.EquipmnetID = key.EquipmnetID
                 inc.PriceID = key.PriceID
                 inc.AreaName = key.AreaName
