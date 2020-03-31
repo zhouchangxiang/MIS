9e6fd1593dd641088654c96044045ab76e675ec2
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2020-03-31 10:46:28 +0800

    能耗看板趋势图

--- handlers/energymanager/energy_manager.py
+++ handlers/energymanager/energy_manager.py
@@ -919,3 +919,35 @@ def exportx(Area, EnergyClass, CurrentTime):
     writer.close()
     output.seek(0)
     return output
+
+@energy.route('/trendlookboard', methods=['POST', 'GET'])
+def trendlookboard():
+    '''
+    能耗看板的能耗趋势
+    :return:
+    '''
+    if request.method == 'GET':
+        data = request.values
+        try:
+            dir = {}
+            EnergyClass = data.get("EnergyClass")
+            CompareTime = data.get("CompareTime")
+            oclass = db_session.query(TagDetail).filter(TagDetail.TagClassValue == EnergyClass).all()
+            oc_list = []
+            for oc in oclass:
+                oc_list.append(oc.TagClassValue)
+            rows_list = []
+            for i in range(0,24):
+                dir_rows = {}
+                start = CompareTime +" "+ addzero(i) + ":00:00"
+                dir_rows["时间"] = start
+                end = CompareTime + " " + addzero(i) + ":59:59"
+                count = energyStatistics(oc_list, start, end, EnergyClass)
+                dir_rows["能耗量"] = count
+                rows_list.append(dir_rows)
+            dir["rows"] = rows_list
+            return json.dumps(dir, cls=AlchemyEncoder, ensure_ascii=False)
+        except Exception as e:
+            print(e)
+            logger.error(e)
+            insertSyslog("error", "能耗看板的能耗趋势查询报错Error：" + str(e), current_user.Name)
