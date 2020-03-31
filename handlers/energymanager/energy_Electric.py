a586974fbf060381399a755d4452a77219366943
beginner <1975436224@qq.com>
2020-03-31 12:36:45 +0800

    完成了分页功能

--- handlers/energymanager/energy_Electric.py
+++ handlers/energymanager/energy_Electric.py
@@ -110,6 +110,7 @@ def strlastMonth(currmonth):
             la = str(las)
         return str0 + "-" + la
 
+
 def energyElectricSelect(data):
     if request.method == 'GET':
         try:
@@ -579,5 +580,11 @@ def energyElectricHistory():
 def get_electric():
     start_time = request.values.get('StartTime')
     end_time = request.values.get('EndTime')
-    results = db_session.query(ElectricEnergy).filter(ElectricEnergy.CollectionDate.between(start_time, end_time)).all()
-    return json.dumps(results, cls=AlchemyEncoder, ensure_ascii=False)
+    # 当前页数
+    current_page = request.values.get('offset', 1, type=int)
+    # 每页显示条数
+    pagesize = int(request.values.get('limit', 10))
+    results = db_session.query(ElectricEnergy).filter(
+        ElectricEnergy.CollectionDate.between(start_time, end_time)).order_by(ElectricEnergy.ID.desc()).all()
+    data = results[(current_page - 1) * pagesize + 1:current_page * pagesize + 1]
+    return json.dumps({'total': len(results), 'rows': data}, cls=AlchemyEncoder, ensure_ascii=False)
