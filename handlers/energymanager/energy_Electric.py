1812dc8348f80698f21942222af87d3034a3d0db
HuangXiongjin <likehxj@qq.com>
2020-04-02 22:17:51 +0800

    获取电报表的接口编辑

--- handlers/energymanager/energy_Electric.py
+++ handlers/energymanager/energy_Electric.py
@@ -586,9 +586,16 @@ def get_electric():
     pagesize = int(request.values.get('limit'))
     area_name = request.values.get('AreaName')
     if area_name:
-        results = db_session.query(ElectricEnergy).filter(ElectricEnergy.AreaName == area_name).filter(
-            ElectricEnergy.CollectionDate.between(start_time, end_time)).order_by(ElectricEnergy.ID.desc()).all()
-        data = results[(current_page - 1) * pagesize + 1:current_page * pagesize + 1]
+        # results = db_session.query(ElectricEnergy).filter(ElectricEnergy.AreaName == area_name).filter(
+        #     ElectricEnergy.CollectionDate.between(start_time, end_time)).order_by(ElectricEnergy.ID.desc()).all()
+        # data = results[(current_page - 1) * pagesize + 1:current_page * pagesize + 1]
+        tag_list = db_session.query(TagDetail).filter(TagDetail.AreaName == area_name, TagDetail.EnergyClass == '电').all()
+        tag_point = [index.TagClassValue for index in tag_list]
+        sql = "select sum(cast(t1.IncremenValue as decimal(9,2)))*1.2*0.8 as count from [DB_MICS].[" \
+              "dbo].[IncrementElectricTable] t1 where t1.TagClassValue in " + (str(tag_point).replace('[', '(')).replace(']', ')') + " and t1.CollectionDate between '2020-03-25 14:41:54' and '2020-03-25 14:45:54' group by t1.IncremenType"
+        result = db_session.execute(sql).fetchall()
+
+        return json.dumps({'综合': str(round(result[0]['count'], 2))}, cls=AlchemyEncoder, ensure_ascii=False)
     else:
         results = db_session.query(ElectricEnergy).filter(
             ElectricEnergy.CollectionDate.between(start_time, end_time)).order_by(ElectricEnergy.ID.desc()).all()
