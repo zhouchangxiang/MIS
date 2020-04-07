5231f832abcd4e636b73ffd5b115bd4c20756007
ligang <2228695510@qq.ccom>
2020-04-07 10:35:11 +0800

    日期设备在线公共模块抽离

--- webmobile/webproject/src/components/region/Region.vue
+++ webmobile/webproject/src/components/region/Region.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="show-box">
-          <ve-line :data="chartData" height="300px"></ve-line>
+         <DateC></DateC>
+         <ShowNumber></ShowNumber>
        </div>
 </template>
 <script>
+import DateC from '../common/Choosedate.vue'
+import ShowNumber from '../common/Shownumber.vue'
 export default {
     data(){
         return {
-          chartData: {
-          columns: ['日期', '访问用户', '下单用户', '下单率'],
-          rows: [
-            { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
-            { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
-            { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
-            { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
-            { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
-            { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
-          ]
+          radio1:1,
+          radio2:1
         }
-        }
+    },
+    components:{
+      DateC,
+      ShowNumber
+    },
+    methods:{
+      myc(){
+        this.$toast(this.radio1)
+      }
     }
 }
 </script>
@@ -29,5 +32,6 @@ export default {
         width: 375px;
         box-sizing: border-box;
         padding: 0 12px 12px 13px;
+
     }
 </style>
